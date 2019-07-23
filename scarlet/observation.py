78ad5bd334eb040d9fd6fcd50d5d98a1cb04809d
herjy <remy.joseph11@gmail.com>
2019-07-23 15:52:33 -0400

    frame matching for unions. Not working version yet.

--- scarlet/observation.py
+++ scarlet/observation.py
@@ -26,7 +26,7 @@ def _centered(arr, newshape):
 
 
 class Frame():
-    """Spatial and spectral characteristics of the data
+    """Spatial and spectral characteristics of the model
 
     Attributes
     ----------
@@ -60,10 +60,60 @@ class Frame():
                 psfs = psfs.astype(dtype)
 
         self._psfs = psfs
-
         assert channels is None or len(channels) == shape[0]
         self.channels = channels
         self.dtype = dtype
+
+    def combine_observations(self, observations):
+        """Creates a common frame for two observations sets
+
+        This function modifies the frame so that it can cover the full spatial and spectral range of several observations.
+        The wcs of the frame is updated with the shape that encapsulates all observations.
+        The reference pixel of the wcs is set to correspond with the center of the observation chosen to match the frame.
+
+        Attributes
+        ----------
+        observations: array
+            an array of observations that are to be matched in a commen frame.
+
+        """
+
+        assert self.wcs in [obs.wcs for obs in observations], \
+            "The wcs of the frame should match the pwcs of at least one observation"
+
+        corners_obs = []
+        channels_frame = []
+        for obs in observations:
+            coord = np.where(np.zeros((obs.images.shape[1], obs.images.shape[2])) == 0)
+            if obs.wcs.naxis == 3:
+                Ra, Dec, l = obs.wcs.all_pix2world(coord[0], coord[1], 0, 0)
+            elif obs.wcs.naxis == 2:
+                Ra, Dec = obs.wcs.all_pix2world(coord[0], coord[1], 0)
+            #Coordinates of the observation in the frame's frame
+            x_frame, y_frame = self.wcs.all_world2pix(Ra, Dec, 0)
+            corners_obs.append([y_frame.min(), y_frame.max(), x_frame.min(), x_frame.max()])
+            #Channels of the model frame
+            channels_frame += obs.channels
+
+        #Corner of the frame that encapsulates all observations
+        corners_min = np.min(corners_obs, axis=0)
+        corners_max = np.max(corners_obs, axis=0)
+
+        #updates the shape, channels and wcs of the newly formed frame
+        self._shape = (len(channels_frame), np.int(corners_max[1]-corners_min[0]),
+                       np.int(corners_max[3]-corners_max[2]))
+
+
+        self.wcs.array_shape = (self._shape[1], self.shape[2])
+        self.wcs.crpix = (np.int(self._shape[1]-corners_min[0]), np.int(self._shape[2]-corners_min[1]))
+
+        self.channels = channels_frame
+
+        print(self.wcs.array_shape, self.wcs.crpix, (np.int(self._shape[1]-corners_min[0]), np.int(self._shape[2]-corners_min[1])))
+        [obs.match(self) for obs in observations]
+        return self
+
+
 
     @property
     def C(self):
@@ -111,6 +161,7 @@ class Frame():
         return tuple(int(coord) for coord in sky_coord)
 
 
+
 class Observation():
     """Data and metadata for a single set of observations
 
@@ -155,7 +206,7 @@ class Observation():
             prevent artifacts from the FFT.
         """
         self.frame = Frame(images.shape, wcs=wcs, psfs=psfs, channels=channels, dtype=images.dtype)
-
+        self.wcs = wcs
         self.images = np.array(images)
         if weights is not None:
             self.weights = np.array(weights)
@@ -163,6 +214,7 @@ class Observation():
             self.weights = 1
 
         self._padding = padding
+        self.channels = channels
 
     def match(self, model_frame):
         """Match the frame of `Blend` to the frame of this observation.
@@ -190,6 +242,29 @@ class Observation():
                 self.weights = self.weights.astype(model_frame.dtype)
             if self.frame._psfs is not None:
                 self.frame._psfs = self.frame._psfs.astype(model_frame.dtype)
+
+
+        print('zezette')
+        print(self.frame.wcs, model_frame.wcs)
+        if self.frame.wcs != model_frame.wcs:
+            #If frame and model are not in agreement, images is padded to the frame's shape
+            assert np.all(self.frame.wcs.wcs.pc == model_frame.wcs.wcs.pc), \
+                "frame and observation should have the same pixel scale and orientation"
+            if self.frame.wcs.array_shape != model_frame.wcs.array_shape:
+                print('zizi')
+                #frame center:
+                coordf = self.frame.wcs.wcs.crpix
+                #Reference observation center
+                coordr = model_frame.wcs.wcs.crpix
+                shift = np.abs(np.int(coordf[0]-coordr[0])), np.abs(np.int(coordf[1]-coordr[1]))
+                pad_size = (np.array(model_frame.wcs.array_shape[1:]) -
+                            np.array(self.frame.wcs.array_shape[1:])).astype(int)//2
+
+                print(pad_size, shift)
+                self.images = np.pad(self.images, ((0,0), (pad_size[0]+shift[0], pad_size[0]-shift[0]),
+                                                   (pad_size[1]+shift[1], pad_size[1]-shift[1])), 'constant')
+
+
 
         #  channels of model that are represented in this observation
         self._band_slice = slice(None)
@@ -299,12 +374,14 @@ class Observation():
 
 class LowResObservation(Observation):
 
-    def __init__(self, images, wcs=None, psfs=None, weights=None, channels=None, padding=3):
+    def __init__(self, images, psfs=None, weights=None, wcs=None, channels=None, padding=3, perimeter = 'overlap'):
 
+        self.perimeter = perimeter
         assert wcs is not None, "WCS is necessary for LowResObservation"
         assert psfs is not None, "PSFs are necessary for LowResObservation"
+        assert perimeter in ['overlap', 'union'], "perimeter should be either 'overlap' or 'union'."
 
-        super().__init__(images, wcs=wcs, psfs=psfs, weights=weights, channels=channels, padding=padding)
+        super().__init__(images, psfs=psfs, weights=weights, wcs=wcs, channels=channels, padding=padding)
 
     def make_operator(self, shape, psf):
         '''Builds the resampling and convolution operator
@@ -324,14 +401,15 @@ class LowResObservation(Observation):
             the convolution-resampling matrix
         '''
         B, Ny, Nx = shape
-        y_hr, x_hr = np.where(np.zeros((Ny, Nx)) == 0)
         y_lr, x_lr = self._coord_lr
         mat = np.zeros((Ny * Nx, x_lr.size))
+
 
         import scipy.signal as scp
 
         for m in range(np.size(x_lr)):
-            mat[:, m] = scp.fftconvolve(self._ker[m], psf, mode='same').flatten() / np.pi * y_lr.size / y_hr.size
+            mat[:, m] = scp.fftconvolve(self._ker[m], psf, mode='same').flatten()
+            mat[:, m] /= np.sum(mat[:,m])
         return mat
 
     def match_psfs(self, psf_hr, wcs_hr):
@@ -379,14 +457,13 @@ class LowResObservation(Observation):
             psf_wcs_lr.wcs.crpix = ny_lr / 2., nx_lr / 2., 0
 
         mask, p_lr, p_hr = resampling.match_patches(psf_hr.shape, psf_lr.data.shape[1:], psf_wcs_hr, psf_wcs_lr)
+        assert mask.sum() > 0, 'no overlap found between frame and observation. Please check WCSes.'
 
         cmask = np.where(mask == 1)
-
         n_p = np.int((np.size(cmask[0])) ** 0.5)
 
         psf_match_lr = interpolation.sinc_interp(cmask, p_hr[::-1],
                                                  psf_lr.reshape(npsf, ny_lr * nx_lr)).reshape(npsf, n_p, n_p)
-
 
         psf_match_hr = psf_hr[np.int((ny_hr - n_p) / 2):np.int((ny_hr + n_p) / 2),
                        np.int((nx_hr - n_p) / 2):np.int((nx_hr + n_p) / 2)]
@@ -415,7 +492,9 @@ class LowResObservation(Observation):
             self._band_slice = slice(bmin, bmax+1)
 
         # Get pixel coordinates in each frame.
-        mask, coord_lr, coord_hr = resampling.match_patches(model_frame.shape, self.frame.shape, model_frame.wcs, self.frame.wcs)
+        mask, coord_lr, coord_hr = resampling.match_patches(model_frame.shape, self.frame.shape, model_frame.wcs,
+                                                                self.frame.wcs, perimeter = self.perimeter)
+
         self._coord_lr = coord_lr
         self._coord_hr = coord_hr
         self._mask = mask
@@ -453,7 +532,7 @@ class LowResObservation(Observation):
             kernel = kernel[:, 1:, 1:]
 
         kernel = _centered(kernel, observed_psf.shape)
-        diff_psf = kernel / kernel.max()
+        diff_psf = kernel / kernel.sum()
 
         # Computes the resampling/convolution matrix
         resconv_op = []
--- scarlet/resampling.py
+++ scarlet/resampling.py
@@ -36,7 +36,7 @@ def conv2D_fft(shape, coord_lr):
 
     return ker
 
-def match_patches(shape_hr, shape_lr, wcs_hr, wcs_lr):
+def match_patches(shape_hr, shape_lr, wcs_hr, wcs_lr, perimeter = 'overlap'):
     '''Matches datasets at different resolutions
 
     Finds the region of overlap between two datasets and creates a mask for the region as well as the pixel coordinates
@@ -48,6 +48,9 @@ def match_patches(shape_hr, shape_lr, wcs_hr, wcs_lr):
         shapes of the two datasets
     wcs_hr, wcs_lr: WCS objects
         WCS of the Low and High resolution fields respectively
+    mode: string
+        if set to 'overlap', matches the coordinates of pixels overlapping in both fields.
+        if set to 'union', matches the coordinates of all the pixels in both observations.
 
     Returns
     -------
@@ -59,19 +62,21 @@ def match_patches(shape_hr, shape_lr, wcs_hr, wcs_lr):
         coordinates of the overlaps at low resolution in the high resolution frame.
     '''
 
+    assert perimeter in ['overlap', 'union']
+
     if np.size(shape_hr) == 3:
         B_hr, Ny_hr, Nx_hr = shape_hr
     elif np.size(shape_hr) == 2:
         Ny_hr, Nx_hr = shape_hr
     else:
-        raise ValueError('Wrong dimensions for reference image')
+        raise ValueError('Wrong dimensions for reference image. Should be (Ny, Nx) or (B, Ny, Nx)')
 
     if np.size(shape_lr) == 3:
         B_lr, Ny_lr, Nx_lr = shape_lr
     elif np.size(shape_lr) == 2:
         Ny_lr, Nx_lr = shape_lr
     else:
-        raise ValueError('Wrong dimensions for low resolution image')
+        raise ValueError('Wrong dimensions for low resolution image. Should be (Ny, Nx) or (B, Ny, Nx)')
 
     assert wcs_hr != None
     assert wcs_lr != None
@@ -84,10 +89,12 @@ def match_patches(shape_hr, shape_lr, wcs_hr, wcs_lr):
     Y_lr, X_lr = np.where(im_lr == 0)
 
     #Corresponding angular positions
+    #of the low resolution pixels
     if np.size(wcs_lr.array_shape) == 2:
         ra_lr, dec_lr = wcs_lr.all_pix2world(X_lr, Y_lr, 0, ra_dec_order=True)
     elif np.size(wcs_lr.array_shape) == 3:
         ra_lr, dec_lr = wcs_lr.all_pix2world(X_lr, Y_lr, 0, 0, ra_dec_order=True)
+    #of the high reolution pixels
     if np.size(wcs_hr.array_shape) == 2:
         ra_hr, dec_hr = wcs_hr.all_pix2world(y_hr, x_hr, 0, ra_dec_order=True)
     elif np.size(wcs_hr.array_shape) == 3:
@@ -97,7 +104,9 @@ def match_patches(shape_hr, shape_lr, wcs_hr, wcs_lr):
     if np.size(wcs_hr.array_shape) == 2:
         X_hr, Y_hr = wcs_hr.all_world2pix(ra_lr, dec_lr, 0, ra_dec_order=True)
     elif np.size(wcs_hr.array_shape) == 3:
-        X_hr, Y_hr = wcs_hr.all_world2pix(ra_lr, dec_lr, 0, 0, ra_dec_order=True)
+        X_hr, Y_hr, l = wcs_hr.all_world2pix(ra_lr, dec_lr, 0, 0, ra_dec_order=True)
+
+    #At this point, we can compute the
 
     # Coordinates of the high resolution pixels in the low resolution frame
     if np.size(wcs_lr.array_shape) == 2:
@@ -106,30 +115,38 @@ def match_patches(shape_hr, shape_lr, wcs_hr, wcs_lr):
     elif np.size(wcs_lr.array_shape) == 3:
         x_lr, y_lr, l = wcs_lr.all_world2pix(ra_hr, dec_hr, 0, 0, ra_dec_order=True)
 
-    # Mask of low resolution pixels in the overlap at low resolution:
-    over_lr = ((X_hr > 0) * (X_hr < Nx_hr) * (Y_hr > 0) * (Y_hr < Ny_hr))
-    # Mask of low resolution pixels in the overlap at high resolution:
-    over_hr = ((x_lr > 0) * (x_lr < Nx_lr) * (y_lr > 0) * (y_lr < Ny_lr))
 
-    mask = over_hr.reshape(Ny_hr, Nx_hr)
+    if perimeter == 'overlap':
+        # Mask of low resolution pixels in the overlap at low resolution:
+        over_lr = ((X_hr > 0) * (X_hr < Nx_hr) * (Y_hr > 0) * (Y_hr < Ny_hr))
+        # Mask of low resolution pixels in the overlap at high resolution:
+        over_hr = ((x_lr > 0) * (x_lr < Nx_lr) * (y_lr > 0) * (y_lr < Ny_lr))
 
-    class SourceInitError(Exception):
-        """
-        Datasets do not match, no overlap found. Check the coordinates of the observations or the WCS.
-        """
-        pass
+        mask = over_hr.reshape(Ny_hr, Nx_hr)
 
-    if np.sum(mask) == 0:
-        raise SourceInitError
+        class SourceInitError(Exception):
+            """
+            Datasets do not match, no overlap found. Check the coordinates of the observations or the WCS.
+            """
+            pass
 
-    # Coordinates of low resolution pixels in the overlap at high resolution:
-    ylr_lr = X_lr[(over_lr == 1)]
-    xlr_lr = Y_lr[(over_lr == 1)]
-    coordlr_lr = (xlr_lr, ylr_lr)
-    # Coordinates of low resolution pixels in the overlap at low resolution:
-    ylr_hr = X_hr[(over_lr == 1)]
-    xlr_hr = Y_hr[(over_lr == 1)]
-    coordlr_hr = (xlr_hr, ylr_hr)
+        if np.sum(mask) == 0:
+            raise SourceInitError
+
+        # Coordinates of low resolution pixels in the overlap at high resolution:
+        xlr_lr = X_lr[(over_lr == 1)]
+        ylr_lr = Y_lr[(over_lr == 1)]
+        coordlr_lr = (ylr_lr, xlr_lr)
+        # Coordinates of low resolution pixels in the overlap at low resolution:
+        xlr_hr = X_hr[(over_lr == 1)]
+        ylr_hr = Y_hr[(over_lr == 1)]
+        coordlr_hr = (ylr_hr, xlr_hr)
+
+    elif perimeter == 'union':
+        coordlr_lr = (Y_lr, X_lr)
+        coordlr_hr = (Y_hr, X_hr)
+        mask = np.ones((Ny_hr, Nx_hr))
+
 
 
     return mask, coordlr_lr, coordlr_hr
